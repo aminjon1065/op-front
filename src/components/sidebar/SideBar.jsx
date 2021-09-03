41fc25ca588023fba7f89a978e92e8be7e161085
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2021-09-03 16:03:44 +0500

    0.2

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Badge, Image, ListGroup, Nav, Navbar} from "react-bootstrap";
+import {Badge, Button, Image, InputGroup, ListGroup, Nav, Navbar} from "react-bootstrap";
 import logo from "../../logo.svg";
 import {Link, useLocation} from "react-router-dom";
 
@@ -14,16 +14,37 @@ const SideBar = () => {
                             src={logo}
                         />
                     </div>
+                    <InputGroup
+                       size="sm"
+                       className="ps-1"
+                    >
+                        <div
+                            className="search"
+                        >
+                            <input
+                                type="text"
+                                className="search-input"
+                                placeholder="Search..."
+                                name=""
+                            />
+                            <Button
+                                variant="teal"
+                                href="#"
+                                className="search-icon"
+                            ><i className="fa fa-search"></i>
+                            </Button>
+                        </div>
+                    </InputGroup>
                     <ListGroup variant="flush" activeKey={location.pathname}>
                         <Navbar bg="sidebar" variant="dark">
                             <Nav className="flex-column">
-                                <Nav.Link as={Link} eventKey="/" to="/"> Home  <Badge pill bg="light"><i className="fas fa-home text-dark"></i></Badge></Nav.Link>
+                                <Nav.Link as={Link} eventKey="/" to="/"> Home <Badge pill bg="light"><i
+                                    className="fas fa-home text-dark"></i></Badge></Nav.Link>
                                 <Nav.Link as={Link} eventKey="/login" to="/login">Link</Nav.Link>
                                 <Nav.Link eventKey="link-2">Link</Nav.Link>
                             </Nav>
                         </Navbar>
                     </ListGroup>
-
                 </div>
             </div>
         </>
